c35c5850d095b56fcecf07587aed435b94897323
Jat <jat@sinosky.org>
2020-06-10 05:23:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linux Driver for Magewell Pro Capture Family
+
+[Download](https://www.magewell.com/downloads/pro-capture#/driver/linux-x86)
---
+++ src/public/ProductVer.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define VER_MAJOR						1
+#define VER_MINOR						3
+#define VER_RELEASE						0
+#define VER_BUILD						4177
+#define VER_DATE						"2020-04-09 16:48:54"
+#define VER_CLASS		  "RC"
